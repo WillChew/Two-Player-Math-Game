bf8e613c7708cfd566c830b5263e4844742807ab
WillChew <williamchew95@gmail.com>
2018-06-11 20:17:15 -0400

    Initial Commit

---
+++ Two Player Math Game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Two Player Math Game
+//
+//  Created by Will Chew on 2018-06-11.
+//  Copyright © 2018 Will Chew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
